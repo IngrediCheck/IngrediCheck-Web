37ac5a485a9b1da0ece0a6928f5c42ece5daeff6
justanotheratom <sanketpatel.1805@gmail.com>
2024-05-15 09:47:38 -0700

    Update Privacy Policy

--- src/components/screen/PrivacyPolicy/PrivacyTerms.tsx
+++ src/components/screen/PrivacyPolicy/PrivacyTerms.tsx
@@ -5,155 +5,108 @@ const PrivacyTerms = () => {
   return (
     <section>
       <div className="container px-[1.6rem] md:pl-[6.4rem] pb-[10rem] md:pb-[10.9rem]">
-        <div className="flex flex-col gap-y-[3.1rem] [&_h3]:font-inter [&_h3]:font-bold [&_h3]:text-[2rem] [&_h3]:md:text-[2.4rem] [&_h3]:text-dark [&_p]:font-inter [&_p]:text-[1.4rem] [&_p]:text-dark ">
-          {/* term 1 */}
+        <div className="flex flex-col gap-y-[3.1rem] [&_h3]:font-inter [&_h3]:font-bold [&_h3]:text-[2rem] [&_h3]:md:text-[2.4rem] [&_h3]:text-dark [&_h4]:font-inter [&_h4]:font-bold [&_h4]:text-[1.5rem] [&_h4]:md:text-[1.8rem] [&_h4]:text-dark [&_p]:font-inter [&_p]:text-[1.4rem] [&_p]:text-dark ">
+
           <div>
-            <h3>I. Introduction</h3>
+            <p>
+                This Privacy Policy is a contract entered into between You ("you," "your," "yours") and FUNGEE LLC ("we," "us," "our," "FUNGEE LLC").
+            </p>
             <p className="mt-[3rem]">
-              Welcome to blaz.ai. We are dedicated to maintaining the privacy
-              and protection of your personal data. This Privacy Policy outlines
-              the information we collect, how we use it, and your rights
-              concerning that information.
+                This policy applies to all information collected or submitted on the IngrediCheck website (the "Site") and application (the "App"). All defined terms have the meaning assigned to them in the Terms of Use.
+            </p>
+            <p className="mt-[3rem]">
+                We limit the collection of personal information to cases where we truly need it or is required for the operation of our products. Personal information is never shared except to comply with the law, to develop and improve our products, or to protect our rights.
             </p>
           </div>
-          {/* term 1 */}
-
-          {/* term 2*/}
           <div>
-            <h3>II. Information We Collect</h3>
-            <p className="mt-[3rem]">
-              While using blaz.ai, we may collect the following information:
-            </p>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Personal identification information: such as name, email
-                address, and organization name.
-              </p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Technical data: such as IP addresses, browser type, operating
-                system, and other related information.
-              </p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Contract data: the specifics of the smart contracts you test
-                with our service (although we endeavor to work with anonymized
-                or non-sensitive data when possible).
-              </p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Usage data: analytics about how you use and interact with our
-                platform.
-              </p>
-            </div>
+            <h3>Information We Collect</h3>
           </div>
-          {/* term 2 */}
-
-          {/* term 3 */}
           <div>
-            <h3>III. How We Use Your Information</h3>
-            <p className="mt-[3rem]">We use the information we collect to: </p>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>Improve and personalize our services.</p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>Address customer service requests and support needs.</p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Aggregate usage analytics to identify trends and improve user
-                experience.
-              </p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>Ensure the security and integrity of our platform.</p>
-            </div>
-            <div className="flex items-center gap-x-[0.8rem]">
-              <GoDotFill className="text-dark" />
-              <p>
-                Communicate updates, promotions, and news about our services.
-              </p>
-            </div>
-          </div>
-          {/* term 3 */}
-
-          {/* term 4 */}
-          <div>
-            <h3>IV. Data Protection</h3>
-            <p className="mt-[3rem]">
-              We implement robust security measures to protect your data,
-              including [specific security methods, e.g., SSL, encryption,
-              regular vulnerability assessments].
+            <h4>User Data</h4>
+            <p className="mt-[2rem]">
+                We use <Link to='https://www.supabase.com' className='underline'>Supabase</Link> as a Backend for storing all user data. Supabase's Privacy Policy can be found <Link to='https://supabase.com/privacy' className="underline">here</Link>.
             </p>
           </div>
-          {/* term 4 */}
-
-          {/* term 5 */}
           <div>
-            <h3>V. Cookies</h3>
-            <p className="mt-[3rem]">
-              We utilize cookies to enhance user experience, track site usage,
-              and facilitate other features. You can choose to disable cookies
-              through your browser settings, but doing so may affect some
-              platform functionalities.
+            <h4>User-Generated Content</h4>
+            <p className="mt-[2rem]">
+                Users contribute content to a "Community Database" by taking pictures of packaged food items. This content is stored in our
+                Backend Database for processing and moderation. The content of the Community Database is made available to all our users.
             </p>
           </div>
-          {/* term 5 */}
-
-          {/* term 6 */}
           <div>
-            <h3>VI. Third-Party Services</h3>
-            <p className="mt-[3rem]">
-              Blaz.ai may integrate with third-party services to enhance its
-              functionalities. These third-party services have their own privacy
-              policies, and we encourage users to review them.
+            <h4>Usage Data</h4>
+            <p className="mt-[2rem]">
+                We use anonymous analytics data provided by Apple.
             </p>
           </div>
-          {/* term 6 */}
-
-          {/* term 7 */}
           <div>
-            <h3>VII. Your Rights</h3>
-            <p className="mt-[3rem]">
-              Depending on your jurisdiction, you may have rights related to
-              your personal information. This can include the right to access,
-              correct, or delete your data. If you wish to exercise any of these
-              rights, please contact us at [Contact Email/Details].
+            <h4>Payment Data and Purchase History</h4>
+            <p className="mt-[2rem]">
+              We use Apple as a payment provider for paid subscriptions. Apple's Privacy Policy can be found <Link to='https://www.apple.com/legal/privacy/en-ww/' className="underline">here</Link>.
             </p>
           </div>
-          {/* term 7 */}
-
-          {/* term 8 */}
           <div>
-            <h3>VIII. Changes to this Policy</h3>
-            <p className="mt-[3rem]">
-              We may periodically update this Privacy Policy. When changes are
-              significant, we will notify users through the platform or via
-              email.
+            <h4>User and Device Identifiers</h4>
+            <p className="mt-[2rem]">
+              We use randomly generated identifiers in the App to anonymously identify devices and users. These identifiers may be synced to your other devices via your iCloud account. These identifiers are associated with data sent to our servers and other third parties, including Supabase and OpenAI.
             </p>
           </div>
-          {/* term 8 */}
-
-          {/* term 9 */}
           <div>
-            <h3>IX. Contact Us</h3>
-            <p className="mt-[3rem]">
-              For any concerns or questions about this Privacy Policy or your
-              data, please reach out to us at: <Link to='mailto:support@ingredicheck.app' className="underline">support@ingredicheck.app</Link>
+            <h4>Technical Basics</h4>
+            <p className="mt-[2rem]">
+              Our server software and any third-party services may store basic technical information, such as your IP address, in temporary memory or logs. This information is used for operation and maintenance purposes.
             </p>
           </div>
-          {/* term 9 */}
+          <div>
+            <h4>Email</h4>
+            <p className="mt-[2rem]">
+              If you email us for support or other feedback, the emails and email addresses will be retained. The email addresses will only be used to reply to the concerns or suggestions raised and will never be used for any marketing purpose.
+            </p>
+          </div>
+          <div>
+            <h3>Information Usage</h3>
+            <p className="mt-[2rem]">
+                We use the information we collect ("Personal Data") to operate and improve the Site, the App, and customer support.
+            </p>
+            <p className="mt-[2rem]">
+                We may disclose your information in response to subpoenas, court orders, or other legal requirements; to exercise our legal rights or defend against legal claims; to investigate, prevent, or take action regarding illegal activities, suspected fraud or abuse, violations of our policies; or to protect our rights and property.
+            </p>
+            <p className="mt-[2rem]">
+                In the future, we may sell to, buy, merge with, or partner with other businesses. In such transactions, user information may be among the transferred assets.
+            </p>
+          </div>
+          <div>
+            <h4>AI Provider</h4>
+            <p className="mt-[2rem]">
+              We use <Link to='https://www.openai.com' className='underline'>OpenAI</Link> as an AI provider for our service. OpenAI's Privacy Policy can be found <Link to='https://openai.com/policies/privacy-policy' className="underline">here</Link>.
+            </p>
+          </div>
+          <div>
+            <h3>International Transfers of Information</h3>
+            <p className="mt-[2rem]">
+                Information may be processed, stored, and used outside of the country in which you are located. Data privacy laws vary across jurisdictions, and different laws may be applicable to your data depending on where it is processed, stored, or used.
+            </p>
+          </div>
+          <div>
+            <h3>Your Consent</h3>
+            <p className="mt-[2rem]">
+                By using the Site or the App, you consent to this privacy policy.
+            </p>
+          </div>
+          <div>
+            <h3>Contacting Us</h3>
+            <p className="mt-[2rem]">
+                If you have questions regarding this privacy policy, you may email privacy@ingredicheck.app.
+            </p>
+          </div>
+          <div>
+            <h3>Changes to This Policy</h3>
+            <p className="mt-[2rem]">
+                From time to time, we may change and/or update this Privacy Policy. We recommend you regularly review this page for updates.
+            </p>
+          </div>
+
         </div>
       </div>
     </section>
